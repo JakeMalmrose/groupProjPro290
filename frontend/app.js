1050677ff9783808d595775a2a0889dcc1532905
JakeMalmrose <jmalmrose@student.neumont.edu>
2024-05-01 11:04:37 -0600

    Updating frontend, and adding stuff to docker compose

--- frontend/app.js
+++ frontend/app.js
@@ -9,19 +9,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
 app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'register.html'));
 });
-// app.get('/login', ...);
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
-// app.get('/register', ...);
-// app.get('/store', ...);
-// app.get('/library', ...);
+app.get('/store', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'store.html'));
+});
+app.get('/library', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'library.html'));
+});
 
-// Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
