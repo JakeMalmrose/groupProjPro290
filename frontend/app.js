9e9d74d25f8f04fe780b8c8b0f5ab62bca64e077
JakeMalmrose <jmalmrose@student.neumont.edu>
2024-05-01 10:12:07 -0600

    Some updated frontend

--- frontend/app.js
+++ frontend/app.js
@@ -10,6 +10,9 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'register.html'));
+});
 // app.get('/login', ...);
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'login.html'));
