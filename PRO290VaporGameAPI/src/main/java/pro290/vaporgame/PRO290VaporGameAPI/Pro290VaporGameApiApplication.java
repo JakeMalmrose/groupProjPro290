665b801a1dd7dc26a8a23e6eea12047c50ae1a04
NaokiKioko <123998300+NaokiKioko@users.noreply.github.com>
2024-05-02 22:35:18 -0600

    Attempted to get dynamo DB running

--- PRO290VaporGameAPI/src/main/java/pro290/vaporgame/PRO290VaporGameAPI/Pro290VaporGameApiApplication.java
+++ PRO290VaporGameAPI/src/main/java/pro290/vaporgame/PRO290VaporGameAPI/Pro290VaporGameApiApplication.java
@@ -2,8 +2,14 @@ package pro290.vaporgame.PRO290VaporGameAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import pro290.vaporgame.PRO290VaporGameAPI.Controller.GameRepository;
 
 @SpringBootApplication
+@EnableJpaRepositories(excludeFilters =
+@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = GameRepository.class))
 public class Pro290VaporGameApiApplication {
 
 	public static void main(String[] args) {
