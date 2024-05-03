442264686d9fdf7add911942554719eafe89c3ba
JakeMalmrose <jmalmrose@student.neumont.edu>
2024-05-03 09:39:45 -0600

    Added placeholder store page functionality

--- frontend-service-go/cmd/server/main.go
+++ frontend-service-go/cmd/server/main.go
@@ -10,17 +10,43 @@ func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
+	http.HandleFunc("/games", handleGames)
+
 	http.HandleFunc("/", handleIndex)
 	http.HandleFunc("/login", handleLogin)
 	http.HandleFunc("/register", handleRegister)
 	http.HandleFunc("/store", handleStore)
 	http.HandleFunc("/library", handleLibrary)
 
+	
+
 	http.ListenAndServe(":3000", nil)
 }
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "index.html", nil)
+}
+
+type Game struct {
+    ID     string
+    Title  string
+    Price  float64
+    Owned  bool
+}
+
+
+func handleGames(w http.ResponseWriter, r *http.Request) {
+    // Create a slice of Game objects (you can replace this with data from DynamoDB later)
+    games := []Game{
+        {ID: "1", Title: "Game 1", Price: 9.99, Owned: true},
+        {ID: "2", Title: "Game 2", Price: 19.99, Owned: false},
+        {ID: "3", Title: "Game 3", Price: 14.99, Owned: true},
+    }
+
+    // Pass the games data to the template
+    renderTemplate(w, "gameslist.html", map[string]interface{}{
+        "Games": games,
+    })
 }
 
 func handleLogin(w http.ResponseWriter, r *http.Request) {
