2ead0323cab39375df0d998aa668cd649279afe7
JakeMalmrose <jmalmrose@student.neumont.edu>
2024-05-02 22:13:18 -0600

    Dockerized go program

--- frontend-service-go/cmd/server/main.go
+++ frontend-service-go/cmd/server/main.go
@@ -1,81 +1,81 @@
 package main
 
 import (
-    "html/template"
-    "net/http"
 	"bytes"
+	"html/template"
+	"net/http"
 )
 
 func main() {
-    fs := http.FileServer(http.Dir("../../static"))
-    http.Handle("/static/", http.StripPrefix("/static/", fs))
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-    http.HandleFunc("/", handleIndex)
-    http.HandleFunc("/login", handleLogin)
-    http.HandleFunc("/register", handleRegister)
-    http.HandleFunc("/store", handleStore)
-    http.HandleFunc("/library", handleLibrary)
+	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/login", handleLogin)
+	http.HandleFunc("/register", handleRegister)
+	http.HandleFunc("/store", handleStore)
+	http.HandleFunc("/library", handleLibrary)
 
-    http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":3000", nil)
 }
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
-    renderTemplate(w, "index.html", nil)
+	renderTemplate(w, "index.html", nil)
 }
 
 func handleLogin(w http.ResponseWriter, r *http.Request) {
-    if r.Method == http.MethodGet {
-        renderTemplate(w, "index.html", map[string]interface{}{
-            "Content": template.HTML(getContentHTML("login.html")),
-        })
-    } else {
-        // Handle the login form submission
-        // ...
-    }
+	if r.Method == http.MethodGet {
+		renderTemplate(w, "index.html", map[string]interface{}{
+			"Content": template.HTML(getContentHTML("login.html")),
+		})
+	} else {
+		// Handle the login form submission
+		// ...
+	}
 }
 
 func handleRegister(w http.ResponseWriter, r *http.Request) {
-    renderTemplate(w, "index.html", map[string]interface{}{
-        "Content": template.HTML(getContentHTML("register.html")),
-    })
+	renderTemplate(w, "index.html", map[string]interface{}{
+		"Content": template.HTML(getContentHTML("register.html")),
+	})
 }
 
 func handleStore(w http.ResponseWriter, r *http.Request) {
-    renderTemplate(w, "index.html", map[string]interface{}{
-        "Content": template.HTML(getContentHTML("store.html")),
-    })
+	renderTemplate(w, "index.html", map[string]interface{}{
+		"Content": template.HTML(getContentHTML("store.html")),
+	})
 }
 
 func handleLibrary(w http.ResponseWriter, r *http.Request) {
-    renderTemplate(w, "index.html", map[string]interface{}{
-        "Content": template.HTML(getContentHTML("library.html")),
-    })
+	renderTemplate(w, "index.html", map[string]interface{}{
+		"Content": template.HTML(getContentHTML("library.html")),
+	})
 }
 
 func getContentHTML(tmpl string) string {
-    t, err := template.ParseFiles("../../templates/" + tmpl)
-    if err != nil {
-        return ""
-    }
+	t, err := template.ParseFiles("templates/" + tmpl)
+	if err != nil {
+		return ""
+	}
 
-    var contentBuf bytes.Buffer
-    err = t.Execute(&contentBuf, nil)
-    if err != nil {
-        return ""
-    }
+	var contentBuf bytes.Buffer
+	err = t.Execute(&contentBuf, nil)
+	if err != nil {
+		return ""
+	}
 
-    return contentBuf.String()
+	return contentBuf.String()
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
-    t, err := template.ParseFiles("../../templates/" + tmpl)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	t, err := template.ParseFiles("templates/" + tmpl)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    err = t.Execute(w, data)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
+	err = t.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
