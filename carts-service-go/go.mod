d3621a385832f9fbb3b8f40fd95f4751a14fde9a
NaokiKioko <123998300+NaokiKioko@users.noreply.github.com>
2024-05-10 09:32:16 -0600

    Cart Service MAybe Functions

--- carts-service-go/go.mod
+++ carts-service-go/go.mod
@@ -1,7 +1,9 @@
 module github.com/Draupniyr/carts-service
 
-go 1.23
+go 1.22
 
 require github.com/aws/aws-sdk-go v1.52.4
 
 require github.com/jmespath/go-jmespath v0.4.0 // indirect
+
+require github.com/google/uuid v1.6.0
--- carts-service-go/main.go
+++ carts-service-go/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
 	"log"
 	"net/http"
@@ -10,11 +11,50 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/google/uuid"
 )
 
 type Cart struct {
-	ID    string   `json:"id"`
-	Games []string `json:"games"`
+	ID    string `json:"ID"`
+	Games []Game `json:"Games"`
+}
+
+type Game struct {
+	GameID string `json:"GameID"`
+	Title  string `json:"Title"`
+	Price  string `json:"Price"`
+	Owned  bool   `json:"Owned"`
+}
+
+type DynamoCart struct {
+	ID    string                     `json:"ID"`
+	Games []*dynamodb.AttributeValue `json:"Games"`
+}
+
+func CartToDynamoDBItem(cart Cart) map[string]*dynamodb.AttributeValue {
+	item := map[string]*dynamodb.AttributeValue{
+		"ID": {
+			S: aws.String(cart.ID),
+		},
+	}
+
+	var games []*dynamodb.AttributeValue
+	for _, game := range cart.Games {
+		gameAttributeValue, err := dynamodbattribute.MarshalMap(game)
+		if err != nil {
+			log.Println("Error marshaling game:", err)
+			return nil
+		}
+		games = append(games, &dynamodb.AttributeValue{M: gameAttributeValue})
+	}
+	item["Games"] = &dynamodb.AttributeValue{L: games}
+
+	return item
+}
+
+
+type CreateCartRequest struct {
+	Games []Game `json:"Games"`
 }
 
 var dynamodbClient *dynamodb.DynamoDB
@@ -35,40 +75,70 @@ func init() {
 }
 
 func main() {
+	http.HandleFunc("/carts/{ID}", CartsHandlerID)
 	http.HandleFunc("/carts", CartsHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func CartsHandlerID(w http.ResponseWriter, r *http.Request) {
+	// Retrieve carts from DynamoDB
+	switch r.Method {
+	case http.MethodGet:
+		getCartsID(w, r)
+	case http.MethodDelete:
+		deleteCartID(w, r)
+	case http.MethodPut:
+		updateCartID(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
 }
 
 func CartsHandler(w http.ResponseWriter, r *http.Request) {
 	// Retrieve carts from DynamoDB
 	switch r.Method {
-	case http.MethodGet:
+	case http.MethodGet: // ADMIN
 		getCarts(w, r)
 	case http.MethodPost:
 		createCart(w, r)
-	case http.MethodDelete:
+	case http.MethodDelete: // ADMIN
 		deleteCart(w, r)
-	case http.MethodPut:
-		updateCart(w, r)
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
+}
 
+func getCartsID(w http.ResponseWriter, r *http.Request) {
+	//get ID from URL
+	id := r.FormValue("ID")
+	// Query the Carts table for the cart with the specified ID
+	carts, err := dynamodbClient.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String("Carts"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"ID": {
+				S: aws.String(id),
+			},
+		},
+	})
+	if err != nil {
+		log.Println("Error getting item from Carts table:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Render the template with the retrieved carts data
+	renderTemplate(w, "cart.html", map[string]interface{}{
+		"Carts": carts,
+	})
 }
 
 func getCarts(w http.ResponseWriter, r *http.Request) {
-	result, err := dynamodbClient.Scan(&dynamodb.ScanInput{
+	// Query the Carts table for all carts
+	carts, err := dynamodbClient.Scan(&dynamodb.ScanInput{
 		TableName: aws.String("Carts"),
 	})
 	if err != nil {
 		log.Println("Error scanning Carts table:", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-	var carts []Cart
-	err = dynamodbattribute.UnmarshalListOfMaps(result.Items, &carts)
-	if err != nil {
-		log.Println("Error unmarshaling carts:", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
@@ -77,35 +147,45 @@ func getCarts(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
+
 func createCart(w http.ResponseWriter, r *http.Request) {
-	cart := Cart{
-		ID:    r.FormValue("id"),
-		Games: []string{},
-	}
-	cart.Games = append(cart.Games, r.FormValue("game"))
-	av, err := dynamodbattribute.MarshalMap(cart)
+	// Parse the request body
+	var createRequest CreateCartRequest
+	err := json.NewDecoder(r.Body).Decode(&createRequest.Games)
 	if err != nil {
-		log.Println("Error marshaling cart:", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Println("Error decoding request body:", err)
+		http.Error(w, "Bad Request", http.StatusBadRequest)
 		return
 	}
+
+	// Generate a new UUID for the cart ID
+	cartID := uuid.New().String()
+
+	newCart := Cart{
+		ID:    cartID,
+		Games: createRequest.Games,
+	}
+
 	_, err = dynamodbClient.PutItem(&dynamodb.PutItemInput{
 		TableName: aws.String("Carts"),
-		Item:      av,
+		// https://gist.github.com/wliao008/e0dba6a3cf089d46932d39b90f9d838f
+		// maybe the answer for this v
+		Item: CartToDynamoDBItem(newCart),
 	})
 	if err != nil {
 		log.Println("Error putting item into Carts table:", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	// http.Redirect(w, r, "/carts", http.StatusSeeOther)
+
 }
-func deleteCart(w http.ResponseWriter, r *http.Request) {
-	id := r.FormValue("id")
+
+func deleteCartID(w http.ResponseWriter, r *http.Request) {
+	id := r.FormValue("ID")
 	_, err := dynamodbClient.DeleteItem(&dynamodb.DeleteItemInput{
 		TableName: aws.String("Carts"),
 		Key: map[string]*dynamodb.AttributeValue{
-			"id": {
+			"ID": {
 				S: aws.String(id),
 			},
 		},
@@ -115,26 +195,50 @@ func deleteCart(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	http.Redirect(w, r, "/carts", http.StatusSeeOther)
 }
 
-func updateCart(w http.ResponseWriter, r *http.Request) {
-	id := r.FormValue("id")
-	game := r.FormValue("game")
-	_, err := dynamodbClient.UpdateItem(&dynamodb.UpdateItemInput{
+func deleteCart(w http.ResponseWriter, r *http.Request) {
+	// Delete all items from the Carts table
+	_, err := dynamodbClient.DeleteItem(&dynamodb.DeleteItemInput{
+		TableName: aws.String("Carts"),
+	})
+	if err != nil {
+		log.Println("Error deleting item from Carts table:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
+
+
+
+func updateCartID(w http.ResponseWriter, r *http.Request) {
+	// get the cart ID from the URL
+	id := r.FormValue("ID")
+	// Parse the request body = []Game
+	var updateRequest []Game
+	err := json.NewDecoder(r.Body).Decode(&updateRequest)
+	if err != nil {
+		log.Println("Error decoding request body:", err)
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	updatedCart := Cart{
+		ID:    id,
+		Games: updateRequest,
+	}
+
+	// update the cart with the new games
+	_, err = dynamodbClient.UpdateItem(&dynamodb.UpdateItemInput{
 		TableName: aws.String("Carts"),
 		Key: map[string]*dynamodb.AttributeValue{
-			"id": {
-				S: aws.String(id),
+			"ID": {
+				S: aws.String(updatedCart.ID),
 			},
 		},
-		UpdateExpression: aws.String("SET #games = list_append(#games, :game)"),
-		ExpressionAttributeNames: map[string]*string{
-			"#games": aws.String("games"),
-		},
 		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
-			":game": {
-				SS: []*string{aws.String(game)},
+			":Games": {
+				L: CartToDynamoDBItem(updatedCart)["Games"].L,
 			},
 		},
 	})
@@ -143,7 +247,6 @@ func updateCart(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	http.Redirect(w, r, "/carts", http.StatusSeeOther)
 }
 
 func renderTemplate(w http.ResponseWriter, templateName string, data interface{}) {
