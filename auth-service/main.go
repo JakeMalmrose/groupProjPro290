abaf33af10ee0edfc8d7ab49c72282c5ded2e425
JakeMalmrose <jmalmrose@student.neumont.edu>
2024-05-16 11:37:22 -0600

    Adding auth service to consul

--- auth-service/main.go
+++ auth-service/main.go
@@ -6,8 +6,12 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+    "os"
+    "strconv"
+    "log"
 
 	"golang.org/x/crypto/bcrypt"
+    "github.com/hashicorp/consul/api"
 
 	database "github.com/Draupniyr/auth-service/database"
 	"github.com/dgrijalva/jwt-go"
@@ -19,12 +23,32 @@ type Claims struct {
     jwt.StandardClaims
 }
 
-var jwtSecret = []byte("your-secret-key")
+var jwtSecret = []byte("zachariah-hansen")
+
+var consulClient *api.Client
 
 func main() {
     http.HandleFunc("/login", loginHandler)
 	http.HandleFunc("/register", registerHandler)
-    http.ListenAndServe(":8080", nil)
+
+
+    err := registerService()
+	if err != nil {
+		log.Fatal("Error registering service with Consul:", err)
+	}
+
+    http.ListenAndServe(":3000", nil)
+}
+
+func init() {
+    var err error
+    consulConfig := api.DefaultConfig()
+    consulConfig.Address = os.Getenv("CONSUL_ADDRESS")
+    consulClient, err = api.NewClient(consulConfig)
+    if err != nil {
+        log.Fatal("Error creating Consul client:", err)
+    }
+
 }
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
@@ -126,3 +150,24 @@ func hashPassword(password string) (string, error) {
     return string(hashedPassword), nil
 }
 
+func registerService() error {
+	serviceName := os.Getenv("SERVICE_NAME")
+	serviceID := os.Getenv("SERVICE_ID")
+	port, _ := strconv.Atoi(os.Getenv("SERVICE_PORT"))
+
+	tags := []string{
+		"TRAEFIK_ENABLE=true",
+		"traefik.http.routers.authservice.rule=PathPrefix(`/auth`)",
+		"TRAEFIK_HTTP_SERVICES_AUTH_LOADBALANCER_SERVER_PORT=3000",
+	}
+
+	service := &api.AgentServiceRegistration{
+		ID:      serviceID,
+		Name:    serviceName,
+		Address: "auth-service",
+		Port:    port,
+		Tags:    tags,
+	}
+
+	return consulClient.Agent().ServiceRegister(service)
+}
